4520d45dac19ad44439c082d91dabd18209fb1e6
BarbaraStanley <barbeaboo@gmail.com>
2022-11-16 10:54:40 +0000

    creating a new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,9 +11,13 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t bnode;
+	binary_tree_t *bnode;
 
 	bnode = malloc(sizeof(binary_tree_t));
+
+	if (!bnode){
+		return (NULL);
+	}
 
 	bnode->n = value;
 	bnode->parent = parent;
