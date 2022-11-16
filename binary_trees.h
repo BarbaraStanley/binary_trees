078d721f9875c6d1b412dcd70a8cd6fb3e2a2386
BarbaraStanley <barbeaboo@gmail.com>
2022-11-16 14:54:37 +0000

    task 5 with header o

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREE_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -26,10 +27,20 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 
 typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+void binary_tree_delete(binary_tree_t *tree);
+
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+int binary_tree_is_root(const binary_tree_t *node);
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
 #endif /*BINARY_TREE_H*/
