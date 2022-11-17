fe8ad35d648a26ff2edf70f13cc5f7f2c8181d58
BarbaraStanley <barbeaboo@gmail.com>
2022-11-17 22:01:59 +0000

    correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,12 +22,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent->left)
 	{
-		parent->left = new_left_child;
 		new_left_child->left = left_child;
 		left_child->parent = new_left_child;
 	}
-	else
-		parent->left = new_left_child;
-		return (new_left_child);
-	return (NULL);
+
+	parent->left = new_left_child;
+	return (new_left_child);
 }
