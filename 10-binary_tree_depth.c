dcf2a221e87e19ffe8fc513e7098e4b1795d724b
BarbaraStanley <barbeaboo@gmail.com>
2022-11-18 01:46:52 +0000

    10, 11, 12

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,5 +9,15 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree)
-		return 0;
+		return (0);
+	
+	size_t depth;
 
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
